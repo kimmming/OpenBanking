eb061bafe928b0c5292f9b687bc205acc5e839ef
jintamsak <wjsguswl310@naver.com>
2022-05-19 01:20:08 +0900

    스타일적용

--- Components/Home.tsx
+++ Components/Home.tsx
@@ -1,36 +1,78 @@
 import React,{Component, ReactElement} from 'react';
 
 import 'react-native-gesture-handler';
-import { StyleSheet,View,Text,Button } from 'react-native';
+import { StyleSheet,View,Text,Button,TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { color } from 'react-native-reanimated';
 
-
+const Color = {
+  purple : "#dda0dd",
+}
+// const Margin = {
+//   marginBottom : 10
+// }
 
 function HomeScreen({navigation}: {navigation: any}) {
   //const navigation = useNavigation();
-
   return (
-    <View style={{
-      flex:1,
-      alignItems:'center',
-      justifyContent:'center',
-    }}>
-          <Text>선택 화면 - 센터인증 또는 자체 인증</Text>
-          <Button
-            title = "센터 인증"
-            onPress={()=>
-              navigation.navigate('center')}
-              //CenterAuthHome으로
-          />
-          <Button
-            title = "자체인증"
-            onPress={()=>
-              navigation.navigate('self')}
-              //SelfAuthHome으로
-          />
+    <View style={styles.container}>
+      <Text style = {styles.text} >선택 화면 - 센터인증 또는 자체 인증</Text>
+      <View style = {styles.margin}>
+        <Button
+          color = {Color.purple}
+          title = "센터 인증"
+          onPress={()=>
+            navigation.navigate('center')}
+            //CenterAuthHome으로
+        />
+        {/* <TouchableOpacity
+          style={styles.btn}
+          onPress={()=>
+            navigation.navigate('center')}
+        >
+          <Text style={styles.btntext}>센터 인증</Text>
+        </TouchableOpacity> */}
       </View>
+      <View style = {styles.margin}>
+        <Button
+          color = {Color.purple}
+          title = "자체 인증"
+          onPress={()=>
+            navigation.navigate('self')}
+            //SelfAuthHome으로
+        />
+      </View>  
+    </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container :{
+    flex:1,
+    padding:10
+    // alignItems:'center',
+    // justifyContent:'center',
+  },
+  text:{
+    fontSize: 15,
+    alignItems:'center',
+    justifyContent:'center',
+    marginBottom:10
+  },
+  margin:{
+    marginBottom:10,
+  },
+  btn:{
+    backgroundColor : "#dda0dd",
+    borderRadius:20,
+    width:100,
+    height:50,
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  btntext:{
+    fontSize:15
+  }
+})
 
 export default HomeScreen;
