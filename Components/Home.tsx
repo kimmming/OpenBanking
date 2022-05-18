d795e4c36092719bb46fbf176bbccd9a3424c849
김민지 <rlaalswl1238@naver.com>
2022-05-19 01:53:41 +0900

    home병합

--- Components/Home.tsx
+++ Components/Home.tsx
@@ -1,42 +1,47 @@
 import React,{Component, ReactElement} from 'react';
 
 import 'react-native-gesture-handler';
-import { StyleSheet,View,Text,Button } from 'react-native';
+import { StyleSheet,View,Text,Button,TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { color } from 'react-native-reanimated';
 
-interface Style {
-  container:any,
-  text:any,
-  centerbtn:any,
+const Color = {
+  purple : "#dda0dd",
 }
+// const Margin = {
+//   marginBottom : 10
+// }
 
 function HomeScreen({navigation}: {navigation: any}) {
   //const navigation = useNavigation();
-
   return (
     <View style={styles.container}>
-      <View >
-      <Text style = {styles.text}>선택 화면 - 센터인증 또는 자체 인증</Text>
-      </View>
-      
-      <View style={styles.centerbtn}>
-        <Button  
-          
-          color="yellow"
+      <Text style = {styles.text} >선택 화면 - 센터인증 또는 자체 인증</Text>
+      <View style = {styles.margin}>
+        <Button
+          color = {Color.purple}
           title = "센터 인증"
-
           onPress={()=>
             navigation.navigate('center')}
             //CenterAuthHome으로
-          
         />
+        {/* <TouchableOpacity
+          style={styles.btn}
+          onPress={()=>
+            navigation.navigate('center')}
+        >
+          <Text style={styles.btntext}>센터 인증</Text>
+        </TouchableOpacity> */}
       </View>
-      <Button
-        title = "자체인증"
-        onPress={()=>
-          navigation.navigate('self')}
-          //SelfAuthHome으로
-      />
+      <View style = {styles.margin}>
+        <Button
+          color = {Color.purple}
+          title = "자체 인증"
+          onPress={()=>
+            navigation.navigate('self')}
+            //SelfAuthHome으로
+        />
+      </View>  
     </View>
   );
 }
@@ -44,16 +49,29 @@ function HomeScreen({navigation}: {navigation: any}) {
 const styles = StyleSheet.create({
   container :{
     flex:1,
+    padding:10
+    // alignItems:'center',
+    // justifyContent:'center',
+  },
+  text:{
+    fontSize: 15,
+    alignItems:'center',
+    justifyContent:'center',
+    marginBottom:10
+  },
+  margin:{
+    marginBottom:10,
+  },
+  btn:{
+    backgroundColor : "#dda0dd",
+    borderRadius:20,
+    width:100,
+    height:50,
     alignItems:'center',
     justifyContent:'center',
   },
-  text:{
-    fontSize: 30
-  },
-  centerbtn:{
-    
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+  btntext:{
+    fontSize:15
   }
 })
 
